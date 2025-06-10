eff7008eda685de5033051e2389a99f62f8f3028
Kishor Hegge <kishor.h@humanfractal.io>
2025-06-10 11:09:12 +0530

    Handle Multiple Route

--- src/app.js
+++ src/app.js
@@ -2,20 +2,38 @@ const express = require('express');
 
 const app = express(); 
 
-app.use("/",(req,res) => {
-    res.send("Hello from the server Home / !");
-
-})
-
-app.use("/hello",(req,res) => {
-    res.send("Hello from the server Hello!");
-
-})
 
 app.use("/test",(req,res) => {
     res.send("Hello from the server Test!");
 
 })
+
+
+app.get("/user", (req,res)=>{
+    res.send({firstName:"Kishor", lastName:"Hegge"});
+});
+
+app.post("/user", (req,res)=>{
+    console.log("Data Posted Successfully")
+    res.send("Data Sent Successfully");
+});
+
+app.delete("/user", (req,res)=>{
+    console.log("Data Deleted Successfully")
+    res.send("Data Deleted Successfully");
+});
+
+// app.use("/",(req,res) => {
+//     res.send("Hello from the server Home / !");
+
+// })
+
+// app.use("/hello",(req,res) => {
+//     res.send("Hello from the server Hello!");
+
+// })
+
+
 
 app.listen(8000 , ()=>{
     console.log("Server is successfully listening on port 8000....");
