1ec6adb3e0d836f644a44c47e1d755f72e9540a9
Kishor Hegge <kishor.h@humanfractal.io>
2025-06-10 11:09:27 +0530

    Handle Routes

--- src/app.js
+++ src/app.js
@@ -2,26 +2,42 @@ const express = require('express');
 
 const app = express(); 
 
-
-app.use("/test",(req,res) => {
-    res.send("Hello from the server Test!");
-
-})
+// app.use("/user", rH1 , rH2, [rH3]);
+// app.use("/user", rH1 , [rH2, rH3],rH4,[rH5]);
+// app.use("/user", [rH1 , rH2, rH3]);
 
 
-app.get("/user", (req,res)=>{
-    res.send({firstName:"Kishor", lastName:"Hegge"});
-});
 
-app.post("/user", (req,res)=>{
-    console.log("Data Posted Successfully")
-    res.send("Data Sent Successfully");
-});
+app.use("/user",[(req,res, next) => {
+    // res.send("Hello from the server 1 Test!");
+    next();
 
-app.delete("/user", (req,res)=>{
-    console.log("Data Deleted Successfully")
-    res.send("Data Deleted Successfully");
-});
+},(req,res, next) => {
+    res.send("Hello from the server 2 Test!");
+    next();
+
+}],(req,res, next) => {
+    res.send("Hello from the server 3 Test!");
+    next();
+
+},)
+
+ 
+
+
+// app.get("/user", (req,res)=>{
+//     res.send({firstName:"Kishor", lastName:"Hegge"});
+// });
+
+// app.post("/user", (req,res)=>{
+//     console.log("Data Posted Successfully")
+//     res.send("Data Sent Successfully");
+// });
+
+// app.delete("/user", (req,res)=>{
+//     console.log("Data Deleted Successfully")
+//     res.send("Data Deleted Successfully");
+// });
 
 // app.use("/",(req,res) => {
 //     res.send("Hello from the server Home / !");
